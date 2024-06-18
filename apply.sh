2222a8b314e949a262dd829e4079755084a0bb4b
sdeeg <sdeeg@planet10.us>
2024-06-18 15:16:21 -0700

    remove platform config

--- apply.sh
+++
@@ -1,5 +0,0 @@
-tanzu apps workload apply eightball \
-      --local-path /Users/sdeeg/Customers/SOI/eightball-kotlin/ \
-      --build-env "BP_JVM_VERSION=21" \
-      --type web \
-      --label "networking.knative.dev/disable-external-domain-tls=true"
--- config/workload.sh
+++
@@ -1,8 +0,0 @@
-tanzu apps workload create eightball \
-      --local-path /Users/sdeeg/Customers/SOI/eightball-kotlin/ \
-      --build-env "BP_JVM_VERSION=21" \
-      --type server
-#      --label "networking.knative.dev/disable-external-domain-tls=true"
-#      --label "networking.knative.dev/visibility=cluster-local"
-
-#networking.knative.dev/disable-external-domain-tls: true
